739f7ef456e69da6319d980cf85b7d5308eec003
Nick Wylynko <nickwylynko@clerk.dev>
2025-03-22 01:45:03 +0800

    publishable key doesn't need to manually be passed through

--- app/clerk-provider/_layout.tsx
+++ app/clerk-provider/_layout.tsx
@@ -1,17 +1,10 @@
-import { ClerkProvider } from '@clerk/clerk-expo'
-import { Slot } from "expo-router"
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { Slot } from "expo-router";
 
 export default function Layout() {
-
-  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
-
-  if (publishableKey === undefined) {
-    throw new Error('Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY to your .env file')
-  }
-
   return (
-    <ClerkProvider publishableKey={publishableKey}>
+    <ClerkProvider>
       <Slot />
     </ClerkProvider>
-  )
+  );
 }
